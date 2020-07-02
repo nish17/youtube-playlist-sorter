38718a458d723c7da28b8e4e17b2d6b1a449d085
Nimesh Solanki <nimesh3536@gmail.com>
2020-07-03 00:59:15 +0530

    add helper functions

---
+++ src/helperFunction.js
@@ -0,0 +1,24 @@
+const url = require('url');
+
+/**
+ * Sorts descending based on viewCount / popularity
+ */
+function comparatorForViews(a, b) {
+  return b.statistics.viewCount - a.statistics.viewCount;
+}
+
+/**
+ * Sorts descending based on likeCount / popularity
+ */
+
+function comparatorForLikes(a, b) {
+  return b.statistics.likeCount - a.statistics.likeCount;
+}
+
+function getPlaylistId(playlistUrl) {
+  const parsedUrl = url.parse(playlistUrl, true);
+  if (!parsedUrl.host.includes('youtube.com')) return undefined;
+  return parsedUrl.query.list;
+}
+
+module.exports = { comparatorForViews, comparatorForLikes, getPlaylistId };
