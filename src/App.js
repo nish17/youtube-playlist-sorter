e14c14b82e0eeb3706b69cc691d5d656d0241e89
Nimesh Solanki <nimesh3536@gmail.com>
2020-07-03 23:17:52 +0530

    remove youtube image

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Login from './Login';
+// import Login from './Login';
 import Dashboard from './Dashboard';
-import yt from './assets/yt.png';
 const App = () => {
   return (
     <section className='main-section'>
       <div className='container'>
-        {/* <header> */}
-          {/* <img className='logo' src={yt} /> */}
+        <div className='main-header'>
           <h1 className='main-title'>Let's sort it!</h1>
-        {/* </header> */}
+        </div>
         <Dashboard />
         {/* <Login /> */}
       </div>
