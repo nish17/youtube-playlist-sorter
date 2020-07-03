f8771f4ecfadfcad85c935fa03914a6783dad47a
Nimesh Solanki <nimesh3536@gmail.com>
2020-07-03 18:18:17 +0530

    add yt image

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { render } from 'react-dom';
 import Login from './Login';
 import Dashboard from './Dashboard';
-
+import yt from './assets/yt.png';
 const App = () => {
   return (
-    <section className='main-section' >
+    <section className='main-section'>
       <div className='container'>
-        <h1 className='main-title'>Let's sort it!</h1>
-
+        {/* <header> */}
+          {/* <img className='logo' src={yt} /> */}
+          <h1 className='main-title'>Let's sort it!</h1>
+        {/* </header> */}
         <Dashboard />
         {/* <Login /> */}
       </div>
