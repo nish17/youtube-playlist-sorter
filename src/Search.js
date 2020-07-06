3b7be611b7f18049df9f75f410052c41aa96c40e
Nimesh Solanki <nimesh3536@gmail.com>
2020-07-07 03:04:23 +0530

    use react-bootstrap for input and search button

--- src/Search.js
+++ src/Search.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { getPlaylistId } from './helperFunction';
+import {Button, InputGroup, FormControl} from 'react-bootstrap';
 const Search = ({ submitID }) => {
   const [playlistURL, setURL] = useState('');
 
@@ -12,10 +13,10 @@ const Search = ({ submitID }) => {
 
   return (
     <div className='search-bar-container'>
-      <div className='row'>
+      {/* <div className='row'>
         <div className='col-lg-8'>
-          <div className='input-group input-group-lg'>
-            <input
+          <div className='input-group input-group-lg'> */}
+            {/* <input
               type='text'
               className='form-control'
               aria-label='Large'
@@ -25,17 +26,35 @@ const Search = ({ submitID }) => {
               placeholder='Youtube playlist URL goes here!'
             />
             <span className='input-group-btn'>
-              <button
+              <Button variant='secondary' type='submit' onClick={handleSubmit}>
+                Go!
+              </Button> */}
+
+              <InputGroup className='mb-3' size='lg'>
+                <FormControl
+                  placeholder='Youtube playlist URL goes here!'
+                  value={playlistURL}
+                  onChange={(e) => setURL(e.target.value)}
+                  aria-label='Youtube playlist URL goes here!'
+                  aria-describedby='basic-addon2'
+                />
+                {/* <InputGroup.Append> */}
+                  <Button variant='secondary' onClick={handleSubmit}>
+                    Go!
+                  </Button>
+                {/* </InputGroup.Append> */}
+              </InputGroup>
+              {/* <button
                 className='btn btn-default'
                 type='submit'
                 onClick={handleSubmit}
               >
                 Go!
-              </button>
-            </span>
-          </div>
+              </button> */}
+            {/* </span> */}
+          {/* </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
