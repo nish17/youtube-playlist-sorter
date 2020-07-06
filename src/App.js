3bb3f25df6156c13a4f7a627bd19c0c8c643841e
Nimesh Solanki <nimesh3536@gmail.com>
2020-07-07 03:03:43 +0530

    use new container

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container'
 // import Login from './Login';
 import Dashboard from './Dashboard';
 import Footer from './Footer';
 const App = () => {
   return (
-    <section className='main-section'>
-      <div className='container'>
+    <div className='main-section'>
+      {/* <div className='container'> */}
+      <Container>
         <div className='main-header'>
           <h1 className='main-title'>Let's sort it!</h1>
         </div>
         <Dashboard />
         {/* <Login /> */}
         {/* <Footer /> */}
-      </div>
-    </section>
+      {/* </div> */}
+      </Container>
+    </div>
   );
 };
 
