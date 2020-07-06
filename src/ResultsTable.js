d5bbeb44028d65038d81aea7d7e38729551acfcc
Nimesh Solanki <nimesh3536@gmail.com>
2020-07-06 17:33:37 +0530

    fix table header bug

--- src/ResultsTable.js
+++ src/ResultsTable.js
@@ -10,25 +10,19 @@ const ResultsTable = ({
   likes,
 }) => {
   return (
-    <div className='result-table'>
-      <table className='table'>
-        <tbody>
-          <tr key={key}>
-            <th className='result-sno' scope='row'>
-              {serialNo}
-            </th>
-            <td className='result-thumbnail'>
-              <img src={thumbnail} />
-            </td>
-            <td className='result-title'>
-              <a href={url}>{title}</a>
-            </td>
-            <td className='result-views'>{views}</td>
-            <td className='result-likes'>{likes}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <tr key={key}>
+      <th className='result-sno' scope='row'>
+        {serialNo}
+      </th>
+      <td className='result-thumbnail'>
+        <img src={thumbnail} />
+      </td>
+      <td className='result-title'>
+        <a href={url}>{title}</a>
+      </td>
+      <td className='result-views'>{views}</td>
+      <td className='result-likes'>{likes}</td>
+    </tr>
   );
 };
 
