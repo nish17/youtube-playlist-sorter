54090309baa9fa852d9fbb276f35ae4a2dc92a86
Nimesh Solanki <nimesh3536@gmail.com>
2020-07-06 17:33:21 +0530

    remove useeffect and usestate from results comp.

--- src/Results.js
+++ src/Results.js
@@ -1,62 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ResultsTable from './ResultsTable';
-// import Resul
 
 const abbreviate = require('number-abbreviate');
 
 const Results = ({ videoList }) => {
-  const [videos, setVideos] = useState([]);
-
-  function formatVideos() {
-    const formattedVideos = videoList.map((video, index) => {
-      return (
-        <ResultsTable
-          key={video.id}
-          url={`https://www.youtube.com/watch?v=${video.id}`}
-          serialNo={index + 1}
-          title={video.snippet.title}
-          thumbnail={video.snippet.thumbnails.default.url}
-          views={abbreviate(video.statistics.viewCount, 1)}
-          likes={abbreviate(video.statistics.likeCount, 1)}
-        />
-        // <li key={video.id}>
-        //   <a href={`https://www.youtube.com/watch?v=${video.id}`}>
-        //     {`${index + 1}. ${video.snippet.title} [👀  ${abbreviate(video.statistics.viewCount,1)} / 👍  ${abbreviate(video.statistics.likeCount, 1)}]`}
-        //   </a>
-        // </li>
-      );
-    });
-    setVideos(formattedVideos);
-  }
-
-  useEffect(() => {
-    formatVideos();
-  }, [videos]);
 
   return (
     <div className='results-container'>
-      {/* <h1>Here you go!</h1> */}
-      <thead class='thead-dark'>
-        <tr>
-          <th className='th-sno' scope='col'>
-            #
-          </th>
-          <th className='th-thumbnail' scope='col'>
-            Video Thumbnail
-          </th>
-          <th className='th-title' scope='col'>
-            Video Title
-          </th>
-          <th className='th-views' scope='col'>
-            Views
-          </th>
-          <th className='th-likes' scope='col'>
-            Likes
-          </th>
-        </tr>
-      </thead>
-      <div>{videos}</div>
-      {/* < */}
+      <table className='table'>
+        <thead className='thead-dark'>
+          <tr>
+            <th scope='col'>#</th>
+            <th scope='col'>Video Thumbnail</th>
+            <th scope='col'>Video Title</th>
+            <th scope='col'>Views</th>
+            <th scope='col'>Likes</th>
+          </tr>
+        </thead>
+        <tbody>
+          {videoList.map((video, index) => (
+            <ResultsTable
+              key={video.id}
+              url={`https://www.youtube.com/watch?v=${video.id}`}
+              serialNo={index + 1}
+              title={video.snippet.title}
+              thumbnail={video.snippet.thumbnails.default.url}
+              views={abbreviate(video.statistics.viewCount, 1)}
+              likes={abbreviate(video.statistics.likeCount, 1)}
+            />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
