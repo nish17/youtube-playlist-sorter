c638db4117e23d5ce852beb17d9f70479e41a885
Nimesh Solanki <nimesh3536@gmail.com>
2020-07-07 03:05:54 +0530

    remove comments

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container'
+import Container from 'react-bootstrap/Container';
 // import Login from './Login';
 import Dashboard from './Dashboard';
 import Footer from './Footer';
 const App = () => {
   return (
     <div className='main-section'>
-      {/* <div className='container'> */}
       <Container>
         <div className='main-header'>
           <h1 className='main-title'>Let's sort it!</h1>
         </div>
         <Dashboard />
-        {/* <Login /> */}
-        {/* <Footer /> */}
-      {/* </div> */}
       </Container>
     </div>
   );
