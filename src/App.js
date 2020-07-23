cd8ce63a80259c7ceacfa752ad78c0318023cf78
Nimesh Solanki <nimesh3536@gmail.com>
2020-07-23 20:54:49 +0530

    add google analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { render } from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 // import Login from './Login';
 import Dashboard from './Dashboard';
 import Footer from './Footer';
+import * as ga from './analytics';
+
 const App = () => {
+  ga.initGA();
+  useEffect(() => {
+    ga.logPageView();
+  }, []);
+
   return (
     <div className='main-section'>
       <Container>
