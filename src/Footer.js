a4b0327d4370be9ed0a448f6f0057ccfd3cacb07
Nimesh Solanki <nimesh3536@gmail.com>
2020-07-23 14:29:32 +0530

    fix footer at the bottom

--- src/Footer.js
+++ src/Footer.js
@@ -6,20 +6,22 @@ const Footer = () => {
   }
 
   return (
-    <div className='footer'>
-      <h5 className='love'>
-        Made with <span className='heart'>♥</span> in India
-      </h5>
-      <h5 className='copyright'>
-        © {getYear()}{' '}
-        <a
-          href='https://twitter.com/NimeshS17'
-          onClick={() => ga.logEvent('footer', 'Twitter')}
-        >
-          @NimeshS17
-        </a>
-      </h5>
-    </div>
+    <footer>
+      <div className='footer'>
+        <h5 className='love'>
+          Made with <span className='heart'>♥</span> in India
+        </h5>
+        <h5 className='copyright'>
+          © {getYear()}{' '}
+          <a
+            href='https://twitter.com/NimeshS17'
+            onClick={() => ga.logEvent('footer', 'Twitter')}
+          >
+            @NimeshS17
+          </a>
+        </h5>
+      </div>
+    </footer>
   );
 };
 
