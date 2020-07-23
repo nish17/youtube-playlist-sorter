c36a074087eb65ee3dcfc21168c9229c5e61102b
Nimesh Solanki <nimesh3536@gmail.com>
2020-07-24 01:35:01 +0530

    use web-share api

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -40,3 +40,11 @@ self.addEventListener('activate', (event) => {
     )
   );
 });
+
+self.addEventListener('DOMContentLoaded', () => {
+  const parsedUrl = new URL(window.location);
+  // searchParams.get() will properly handle decoding the values.
+  console.log('Title shared: ' + parsedUrl.searchParams.get('title'));
+  console.log('Text shared: ' + parsedUrl.searchParams.get('text'));
+  console.log('URL shared: ' + parsedUrl.searchParams.get('url'));
+});
