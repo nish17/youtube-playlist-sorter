0b33aabfbcb9a275f21330eee587899e221dd856
Nimesh Solanki <nimesh3536@gmail.com>
2020-08-05 21:34:02 +0530

    move register sw logic to index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,37 @@
-if ('serviceWorker' in navigator) {
+const path = require('path');
+
+const pathsw = path.resolve(__dirname + '/sw.js');
+navigator.serviceWorker
+  .register(pathsw)
+  .then((registration) => {
+    registration.onupdatefound = () => {
+      const installingWorker = registration.installing;
+      if (installingWorker == null) {
+        return;
+      }
+      installingWorker.onstatechange = () => {
+        if (installingWorker.state === 'installed') {
+          if (navigator.serviceWorker.controller) {
+            console.log(
+              'New content is available and will be used when all ' +
+                'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
+            );
+          } else {
+            console.log('Content is cached for offline use.');
+          }
+        }
+      };
+    };
+  })
+  .catch((error) => {
+    console.error('==>Error during service worker registration:', error);
+  });
+window.addEventListener('DOMContentLoaded', () => {
+  const parsedUrl = new URL(window.location);
+  window._youtubePlaylistURL = parsedUrl.searchParams.get('text');
+});
+
+/* if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
       .register('sw.js')
@@ -11,3 +44,4 @@ window.addEventListener('DOMContentLoaded', () => {
   const parsedUrl = new URL(window.location);
   window._youtubePlaylistURL = parsedUrl.searchParams.get('text');
 });
+ */
