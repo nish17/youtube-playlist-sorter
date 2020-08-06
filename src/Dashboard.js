c165ad8cddbd2893e5fcf886e80507c429db9993
Nimesh Solanki <nimesh3536@gmail.com>
2020-08-06 18:21:43 +0530

    make use of useRef to showing errors

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,31 +1,58 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import { getVideoIdsFromPlayList, getVideoDetails } from './api';
 import { comparatorForLikes } from './helperFunction';
 
 import Search from './Search';
 import Results from './Results';
+import Error from './Error';
 
 const Dashboard = () => {
   const [playlistID, setPlaylistID] = useState('');
   const [videos, setVideos] = useState([]);
+  const [error, setError] = useState({ doesExist: false, type: 'NONE' });
+  const [isLoading, setIsLoading] = useState(false);
+  const didMount = useRef(false);
+
   useEffect(() => {
     (async () => {
-      if (playlistID)
+      if (playlistID) {
         try {
+          setIsLoading(true);
           const videoIds = await getVideoIdsFromPlayList(playlistID);
           const videos = await Promise.all(videoIds.map(getVideoDetails));
+          setIsLoading(false);
+          setError({...error,doesExist: false})
           videos.sort(comparatorForLikes);
           setVideos(videos);
         } catch (e) {
+          setIsLoading(false);
+          setError({ doesExist: true, type: 'PLAYLIST_URL_ERROR' });
           console.log('something went-wrong', JSON.stringify(e));
         }
+      } else {
+        if (didMount.current) {
+          setError({ doesExist: true, type: 'PLAYLIST_URL_ERROR' });
+        } else {
+          didMount.current = true;
+        }
+      }
     })();
   }, [playlistID]);
   return (
     <div className='dashboard'>
       <Search submitID={setPlaylistID} />
-      {playlistID != '' && videos.length != 0 && <Results videoList={videos} />}
+      {playlistID != '' && videos.length != 0 && !error.doesExist && (
+        <Results videoList={videos} />
+      )}
+      {isLoading && (
+        <div className='results-container'>
+          <div className='alert alert-info' role='alert'>
+            Loading...
+          </div>
+        </div>
+      )}
+      {error.doesExist && <Error type={error.type} />}
     </div>
   );
 };
