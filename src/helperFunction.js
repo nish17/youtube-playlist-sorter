caee4eb60e15071e7deeca25c23cbfc09b4219d9
Nimesh Solanki <nimesh3536@gmail.com>
2020-08-08 00:38:24 +0530

    remove sorting functions

--- src/helperFunction.js
+++ src/helperFunction.js
@@ -1,28 +1,16 @@
 const url = require('url');
 
-/**
- * Sorts descending based on viewCount / popularity
- */
-function comparatorForViews(a, b) {
-  return b.statistics.viewCount - a.statistics.viewCount;
-}
-
-/**
- * Sorts descending based on likeCount / popularity
- */
-
-function comparatorForLikes(a, b) {
-  return b.statistics.likeCount - a.statistics.likeCount;
-}
-
 function getPlaylistId(playlistUrl) {
   const parsedUrl = url.parse(playlistUrl, true);
   if (!parsedUrl.host.includes('youtube.com')) return undefined;
   return parsedUrl.query.list;
 }
 
-function generateRandomNumber(){
-  return Math.floor((Math.random() * 10) + 1);
+function generateRandomNumber() {
+  return Math.floor(Math.random() * 10 + 1);
 }
 
-module.exports = { comparatorForViews, comparatorForLikes, getPlaylistId, generateRandomNumber };
+module.exports = {
+  getPlaylistId,
+  generateRandomNumber,
+};
